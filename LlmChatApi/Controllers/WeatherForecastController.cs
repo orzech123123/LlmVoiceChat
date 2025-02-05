1958c8bf422656c8cecd834b1b0a5b19fd0ecc2b
michalorzechowski123 <michalorzechowski123@gmail.com>
2025-02-05 12:20:49 +0100

    api etc

---
+++ LlmChatApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+using LlmChatAutomation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LlmChatApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            var chat = new Chat();
+
+            await chat.InitAsync();
+
+            chat.Start();
+
+            Thread.Sleep(2000);
+
+            chat.SendPrompt("hi!");
+
+            Thread.Sleep(2000);
+
+
+
+
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ LlmChatAutomation/SpeechToText.cs
@@ -0,0 +1,63 @@
+﻿using OpenQA.Selenium;
+using SeleniumUndetectedChromeDriver;
+using System.Threading.Tasks;
+
+namespace LlmChatAutomation
+{
+
+    //var speechToText = new SpeechToText();
+
+    //await speechToText.InitAsync();
+
+    //speechToText.Start();
+
+    //Thread.Sleep(2000);
+
+    //speechToText.StartRecording();
+
+    //while (true)
+    //{
+    //    Thread.Sleep(3000);
+    //    Console.WriteLine(speechToText.GetTranscription());
+    //}
+
+    //return;
+//------------------------------
+
+
+public class SpeechToText
+    {
+        private UndetectedChromeDriver _driver;
+
+        public async Task InitAsync()
+        {
+            _driver = UndetectedChromeDriver.Create(driverExecutablePath: await new ChromeDriverInstaller().Auto());
+            _driver.Manage().Window.Size = new System.Drawing.Size(800, 600);
+        }
+
+        public void Start()
+        {
+            _driver.GoToUrl("https://speechtyping.com/voice-typing/voice-to-text-polish");
+        }
+
+        public void StartRecording()
+        {
+            var cookiesButton = _driver.FindElement(By.Id("ez-accept-all"));
+            cookiesButton.Click();
+
+            Thread.Sleep(3000);
+
+            var startButton = _driver.FindElement(By.ClassName("listen"));
+            startButton.Click();
+        }
+
+        public string GetTranscription()
+        {
+            //textareaWrapper
+            var speechDiv = _driver.FindElement(By.ClassName("ql-editor"));
+            //var speechDiv = _driver.FindElement(By.Id("textareaWrapper"));
+            var text = speechDiv.Text;
+            return text;
+        }
+    }
+}
